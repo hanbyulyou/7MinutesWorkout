076b6cddedaf30489ab39fe015c14dfa45bd46e5
HanByul <hanbyul9477@gmail.com>
2023-01-16 13:37:58 +0900

    Initial commit

---
+++ app/src/main/java/kr/tutorials/a7minutesworkout/HistoryEntity.kt
@@ -0,0 +1,10 @@
+package kr.tutorials.a7minutesworkout
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history-table")
+data class HistoryEntity(
+    @PrimaryKey
+    val data:String
+)
---
+++ app/src/main/java/kr/tutorials/a7minutesworkout/WorkOutApp.kt
@@ -0,0 +1,11 @@
+package kr.tutorials.a7minutesworkout
+
+import android.app.Application
+
+class WorkOutApp: Application() {
+
+    val db by lazy{
+        HistoryDatabase.getInstance(this)
+    }
+
+}
